ad4206c774bfc4fee38cb6a2bc7fe3a7b955a9d5
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2022-03-15 17:08:52 +0000

    Update gaussian function and add elipse behaviour

--- Assets/Scripts/LightDetectorGaussScript.cs
+++ Assets/Scripts/LightDetectorGaussScript.cs
@@ -19,12 +19,17 @@ public class LightDetectorGaussScript : LightDetectorScript {
 				return MinY;
 		}
 	
-		output = 1.0f/(stdDev * (float) Math.Sqrt(2*Math.PI)) * (float) Math.Exp(-0.5f*Math.Pow(output - mean, 2)/(float) Math.Pow(stdDev, 2));
+		//output = 1.0f/(stdDev * (float) Math.Sqrt(2*Math.PI)) * (float) Math.Exp(-0.5f*Math.Pow(output - mean, 2)/(float) Math.Pow(stdDev, 2));
 
-		float mx = 1.0f/(stdDev * (float) Math.Sqrt(2*Math.PI)) * (float) Math.Exp(-0.5f*Math.Pow(0, 2)/(float) Math.Pow(stdDev, 2));
+		//float mx = 1.0f/(stdDev * (float) Math.Sqrt(2*Math.PI)) * (float) Math.Exp(-0.5f*Math.Pow(0, 2)/(float) Math.Pow(stdDev, 2));
+
+		//output /= mx;
+
+		output = (stdDev/Mathf.Sqrt(2*Mathf.PI)) * Mathf.Exp(-Mathf.Pow(output-mean, 2) / (2*Mathf.Pow(stdDev, 2)));
 
 		if(inverse)
-			output =  mx - output;
+			output =  1.0f - output;
+
 
 		if(ApplyLimits) {
 			output = (float) Math.Min(output, MaxY);
